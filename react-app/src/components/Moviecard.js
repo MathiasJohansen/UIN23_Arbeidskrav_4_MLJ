4a9b9343021d1e6f1d533f2db4397a4047fbe762
MathiasJohansen <111729300+MathiasJohansen@users.noreply.github.com>
2023-03-27 19:12:10 +0200

    omh halo

--- react-app/src/components/Moviecard.js
+++ react-app/src/components/Moviecard.js
@@ -1,10 +1,13 @@
-export default function Moviecard({img, title, ingress}){
+export default function Moviecard({img, title, release, genre, dir, actor, award}){
     return(
         <article className="movie-card">
             <h3>{title}</h3>
             <img src={img} alt={title} />
-            
-            <p>{ingress}</p>
+            <p>{release}</p>
+            <p>{genre}</p>
+            <p>{dir}</p>
+            <p>{actor}</p>
+            <p>{award}</p>
             <a href="#">Les mer</a>
         </article>
     )
--- react-app/src/components/SearchResults.js
+++ react-app/src/components/SearchResults.js
@@ -1,16 +1,29 @@
+import { data } from "autoprefixer"
 import { useEffect, useState } from "react"
 import Moviecard from "./Moviecard"
 
 export default function Searchresults(){
     const [movies, setMovies] = useState([])
+    const [movieInfo, setMovieInfo] = useState([])
     const [resultat, setResultat] = useState([])
     
     const getMovies = async() => {
-        const response = await fetch ('http://www.omdbapi.com/?i=tt3896198&apikey=4ffa912&s=james-bond')
+        const response = await fetch ('http://www.omdbapi.com/?apikey=4ffa912&type=movie&s=james-bond')
         const data = await response.json()
         setMovies(data?.Search)
+        console.log(data)
     }
-    console.log(movies)
+    <>
+    const getMovieInfo = async() => {
+        const response = await fetch ('http://www.omdbapi.com/?apikey=4ffa912&type=movie&t=james-bond')
+        const data = await response.json()
+        setMovieInfo(data)
+        console.log(data)
+    }
+    useEffect(() =>{
+        getMovieInfo()
+    },[])
+    </>
     
     useEffect(() =>{
         getMovies()
@@ -22,7 +35,7 @@ export default function Searchresults(){
             
 
             {movies?.map((movie, index) =>(
-                <Moviecard key={index} title={movie?.Title} img={movie?.Poster}/>
+                <Moviecard key={index} title={movie?.Title} img={movie?.Poster}  />
             ))}
 
         </article>
